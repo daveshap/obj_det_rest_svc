6c8d4f51fab3a432e866c1c3506d100114d73b43
David Shapiro <daveshap37@gmail.com>
2020-10-12 12:02:56 -0400

    first attempt!

--- faster_rcnn_inception_resnet_v2.py
+++ faster_rcnn_inception_resnet_v2.py
@@ -1,8 +1,12 @@
 import requests
 import json
 import time
-import numpy
-#import tensorflow_hub as hub
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+uuid_seen = list()  # time indexes already seen
 
 
 def fetch_images(ip, port):
@@ -10,12 +14,34 @@ def fetch_images(ip, port):
     return response.json()
 
 
+def send_msg(data, meta, ip, port):
+    payload = {'data':data, 'meta':meta}
+    response = requests.request(method='POST', url='http://%s:%s' % (ip, port), json=payload)
+    print(response.url, response.ok, response.status_code, meta)
+
+
+def convert_from_json(json_str):
+    arr = np.array(json_str)
+    new_arr = np.expand_dims(arr, 0)
+    tensor = tf.convert_to_tensor(new_arr)
+    return tensor
+
+
 def main():
+    detector = hub.load("https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1")
     while True:
         time.sleep(1)
         try:
-            images = fetch_images('127.0.0.1', 9999)
-            print(len(images))
+            images = fetch_images('127.0.0.1', 9999)  # TODO parameterize this
+            #print(len(images))
+            for img in images:
+                if img['uuid'] not in uuid_seen:
+                    image_tensor = convert_from_json(img['data'])
+                    detector_output = detector(image_tensor)
+                    class_ids = detector_output["detection_classes"]
+                    uuid_seen.append(img['uuid'])
+                    meta = {'time':img['time'], 'type':'inference.sense.optical.object.detection.recognition', 'format':'TBD', 'service':svc_name, 'uuid':str(uuid.uuid4()), 'parent_uuid':img['uuid']}
+                    send_msg(class_ids, meta, '127.0.0.1', 9999)
         except Exception as oops:
             print(oops)
 
